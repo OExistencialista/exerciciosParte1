834774d43f3d6f229edc67343e92c3ebb4aad3c0
OExistencialista <134657253+OExistencialista@users.noreply.github.com>
2024-06-24 11:37:23 -0300

    exercicio 3

---
+++ exercicio3/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program{
+    public static void Main(){
+        int[] v = new int[6];
+        int a = 7;
+        int b = a - 6;
+        while(b<a){
+            v[b] = b+a;
+            Console.WriteLine($"v[{b}]:{v[b]}, b:{b}, a:{a}");
+            b+=2;
+        }
+    }
+}
