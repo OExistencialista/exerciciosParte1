dd81aa097f47560c8d2039f57476dd95183091c4
OExistencialista <134657253+OExistencialista@users.noreply.github.com>
2024-06-24 11:36:16 -0300

    exercicio 2

---
+++ exercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main(){
+        Console.Clear();
+        int a = 2;
+        int[] v = new int[6];
+
+        while(a<6){
+            v[a] = 10*a;
+            Console.WriteLine($"a:{a}, v[{a}]:{v[a]}");
+            a++;
+        }
+    }
+}
