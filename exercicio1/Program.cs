eb4211a2c49ece09054aa4626401bca37ffc6e3b
OExistencialista <134657253+OExistencialista@users.noreply.github.com>
2024-06-24 11:35:36 -0300

    exercicio 1

---
+++ exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+class Program
+{
+    public static void Main()
+    {
+        Console.Clear();
+        int[] v = new int[6];
+        int a = 10;
+        int b = 20;
+        int c = (a + b) / 2;
+        c -=40;
+        
+        
+            v[3] = a+b+c;
+            Console.WriteLine($"v[3]:{v[3]}, a:{a}, b:{b}, c:{c}");
+        
+        
+
+
+    }
+
+    
+}
